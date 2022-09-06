01a4732803230507cacd699c62844d3002064a81
Harsh6873 <111336536+Harsh6873@users.noreply.github.com>
2022-09-06 10:36:19 +0530

    Time: 6 ms (60.39%), Space: 45.9 MB (25.74%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,12 +1,24 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j = i+1;j< nums.length;j++){
+        //         if(nums[j] == target - nums[i]){
+        //             return new int[] {i,j};
+        //         }
+        //     }
+        // }
+        // return null;   
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i=0;i<nums.length;i++){
-            for(int j = i+1;j< nums.length;j++){
-                if(nums[j] == target - nums[i]){
-                    return new int[] {i,j};
-                }
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[] {map.get(complement), i};
             }
+            else{
+                map.put(nums[i],i);
+            }
+            
         }
-        return null;   
+        throw new IllegalArgumentException("No two Sum solution");
     }
 }
