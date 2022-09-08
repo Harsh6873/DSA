c6a84f0aafef1553ffb355f3f02056018793bc21
Harsh6873 <111336536+Harsh6873@users.noreply.github.com>
2022-09-08 10:46:39 +0530

    Time: 3 ms (69.38%), Space: 84.3 MB (20.64%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,22 +1,33 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int buy[] = new int[n];
-        int sell[] = new int[n];
-        buy[0] = prices[0];
-        for(int i=1;i<n;i++){
-            buy[i] = Math.min(buy[i-1],prices[i]);
-        }
-        sell[n-1] = prices[n-1];
-        for(int i=n-2;i>=0;i--){
-            sell[i] = Math.max(sell[i+1],prices[i]);
+//         int n = prices.length;
+//         int buy[] = new int[n];
+//         int sell[] = new int[n];
+//         buy[0] = prices[0];
+//         for(int i=1;i<n;i++){
+//             buy[i] = Math.min(buy[i-1],prices[i]);
+//         }
+//         sell[n-1] = prices[n-1];
+//         for(int i=n-2;i>=0;i--){
+//             sell[i] = Math.max(sell[i+1],prices[i]);
+            
+//         }
+//         int max=0;
+//         for(int i=0;i<n;i++){
+//             max = Math.max(max,sell[i] - buy[i]);
+//         }
+        
+//         return max;
+        int minPrices = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i] < minPrices){
+                minPrices = prices[i];
+                
+            }
+            maxProfit = Math.max(prices[i]-minPrices,maxProfit);
             
         }
-        int max=0;
-        for(int i=0;i<n;i++){
-            max = Math.max(max,sell[i] - buy[i]);
-        }
-        
-        return max;
+        return maxProfit;
     }
 }
