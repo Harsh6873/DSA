c5ab6005a2a902c1365dd8b658dea5e0b37828f5
Harsh6873 <111336536+Harsh6873@users.noreply.github.com>
2022-09-12 17:10:52 +0530

    Time: 1 ms (65.44%), Space: 41.7 MB (81.67%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -8,7 +8,7 @@ class Solution {
         for(int i = 0;i<m;i++){
             for(int j =0;j<n;j++){
                 if(i == 0|| j == 0){
-                    if(obstacleGrid[i][j] == 1 || (i!=0 && obstacleGrid[i-1][0] == 0) || (j!=0 && obstacleGrid[i][j-1] == 0)){
+                    if(obstacleGrid[i][j] == 1 || (i!=0 && obstacleGrid[i-1][j] == 0) || (j!=0 && obstacleGrid[i][j-1] == 0)){
                         obstacleGrid[i][j] = 0;
                     }
                     else{
