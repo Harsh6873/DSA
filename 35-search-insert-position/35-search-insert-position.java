eb430c9e201c98568e96006f67046649a500cfc1
Harsh6873 <111336536+Harsh6873@users.noreply.github.com>
2022-09-24 21:06:16 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (7.84%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,13 +1,12 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for(int i =0; i<=nums.length-1;i++){
+        for(int i=0;i<nums.length;i++){
             if(nums[i] == target){
                 return i;
             }
             else if(nums[i] > target){
                 return i;
             }
-            
         }
         return nums.length;
     }
