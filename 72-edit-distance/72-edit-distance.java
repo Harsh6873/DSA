bd1b36fca6367d0d66cd0931d7a44e602a3ef1bb
Harsh6873 <111336536+Harsh6873@users.noreply.github.com>
2022-09-25 10:58:58 +0530

    Time: 9 ms (56.67%), Space: 44.8 MB (59.52%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,42 +1,34 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        char str[] = word1.toCharArray();
-        char str1[] = word2.toCharArray();
-        int dp[][] = new int[str.length+1][str1.length+1];
-       
-//         for(int i=1;i<str.length;i++){
-//             
-//             for(int j=1;j<=str1.length;j++){
-//                 if(i==0 || j==0){
-//                     dp[i][j] = count++; 
-//                 }
-//                 else{
-//                     dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1])) + 1;
-//                 }
-//             }
-            
-//         }
-        for(int i=0;i<dp[0].length;i++){
-            dp[0][i] = i;
-        }
+        char str1[] = word1.toCharArray();
+        char str2[] = word2.toCharArray();
+        
+        int dp[][] = new int[str1.length+1][str2.length+1];
+        dp[0][0] = 0;
+        
         for(int i=0;i<dp.length;i++){
             dp[i][0] = i;
         }
-        for(int i=1;i<=str.length;i++){
-            for(int j=1;j<=str1.length;j++){
-                if(str[i-1] == str1[j-1]){
+        for(int i=0;i<dp[0].length ; i++){
+            dp[0][i] = i;
+        }
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(str1[i-1] == str2[j-1]){
                     dp[i][j] = dp[i-1][j-1];
+                    
                 }
                 else{
-                    dp[i][j] = 1+ min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = 1+minn(dp[i-1][j], dp[i-1][j-1],dp[i][j-1]);
                 }
+                // dp[i][j] = minn(dp[i-1][j], dp[i-1][j-1],dp[i][j-1]);
+                
             }
         }
-        
-        return dp[str.length][str1.length];
+        return dp[str1.length][str2.length];
     }
-    int min(int a, int b, int c){
-        int l = Math.min(a,b);
-        return Math.min(l,c);
+    private int minn(int x,int y, int z){
+        int l = Math.min(x,y);
+        return Math.min(l,z);
     }
 }
