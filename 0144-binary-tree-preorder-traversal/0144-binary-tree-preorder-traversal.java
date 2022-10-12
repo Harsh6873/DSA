45c58d8201447041067a569977e937f8361a291c
Harsh6873 <111336536+Harsh6873@users.noreply.github.com>
2022-10-12 10:40:43 +0530

    Time: 1 ms (49.71%), Space: 42.5 MB (19.34%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -15,24 +15,40 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-//         if(root == null){
-//             return;
-//         }
         List<Integer> li = new ArrayList<Integer>();
-        dfs(root,li);
+        if(root == null) return li;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode temp = stack.pop();
+            // System.out.print(temp.data+" ");
+            li.add(temp.val);
+            if(temp.right != null){
+                stack.push(temp.right);
+            }
+            if(temp.left!= null){
+                stack.push(temp.left);
+            }
+        }
         return li;
-//         li.add(root);
-//         li.add(preorderTraversal(root.left));
+// //         if(root == null){
+// //             return;
+// //         }
+//         List<Integer> li = new ArrayList<Integer>();
+//         dfs(root,li);
+//         return li;
+// //         li.add(root);
+// //         li.add(preorderTraversal(root.left));
         
-//         li.add(preorderTraversal(root.right));
+// //         li.add(preorderTraversal(root.right));
         
         
         
-    }
-    private void dfs(TreeNode node , List<Integer> list){
-        if(node == null) return;
-        list.add(node.val);
-        dfs(node.left,list);
-        dfs(node.right,list);
+//     }
+//     private void dfs(TreeNode node , List<Integer> list){
+//         if(node == null) return;
+//         list.add(node.val);
+//         dfs(node.left,list);
+//         dfs(node.right,list);
     }
 }
