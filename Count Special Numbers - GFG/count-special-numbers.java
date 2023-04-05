55994e7663197839b6f1e5fd1c7a510aa67eadab
Harsh Agarwal <111336536+Harsh6873@users.noreply.github.com>
2023-04-05 08:41:20 +0530

    Added solution - LeetHub

---
+++ Count Special Numbers - GFG/count-special-numbers.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt(); // Inputting the testcases
+        PrintWriter out = new PrintWriter(System.out);
+        while (t-- > 0) {
+
+            int i = 0;
+            int N = sc.nextInt();
+
+            int arr[] = new int[(int)(N)];
+
+            for (i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Solution ob = new Solution();
+            out.println(ob.countSpecialNumbers(N, arr));
+        }
+        out.flush();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int countSpecialNumbers(int N, int arr[]) {
+        // Code here
+        HashSet<Integer> st = new HashSet<>();
+        int res=0;
+        Arrays.sort(arr);
+        int mn = arr[N-1];
+        for(int i=0;i<N;i++){
+            if(st.contains(arr[i])){
+                res++;
+                continue;
+            }
+            else if(i < N-1 && arr[i] == arr[i+1]){
+                res++;
+            }
+            int j=1;
+            while(arr[i]*j <=mn){
+                st.add(arr[i]*j);
+                j++;
+            }
+        }
+        return res;
+    }
+}
