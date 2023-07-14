dc7a3ad7d3c580c43ae670bb086365247109c803
Harsh Agarwal <111336536+Harsh6873@users.noreply.github.com>
2023-07-14 10:45:21 +0530

    Added solution - LeetHub

---
+++ Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class TwoStack
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in); 
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			twoStacks g = new twoStacks();
+			int Q = sc.nextInt();
+			while(Q>0)
+			{
+				int stack_no = sc.nextInt();
+				int QueryType = sc.nextInt();
+				
+				
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					if(stack_no == 1)
+					 g.push1(a);
+					else if(stack_no ==2)
+					 g.push2(a);
+				}else if(QueryType == 2)
+				{
+					if(stack_no==1)
+					System.out.print(g.pop1()+" ");
+					else if(stack_no==2)
+					System.out.print(g.pop2()+" ");
+				}
+			
+				Q--;
+			}
+				System.out.println();
+		  T--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+
+class twoStacks
+{
+    int arr[] = new int[100];
+    int l=-1;
+    int r=100;
+    //Function to push an integer into the stack1.
+    void push1(int x)
+    {
+        arr[++l] = x;
+     
+    }
+    //Function to push an integer into the stack2.
+    void push2(int x)
+    {
+        arr[--r]= x;
+       
+    }
+    //Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        return l < 0?-1:arr[l--];
+        
+    }
+    //Function to remove an element from top of the stack2.
+    int pop2()
+    {
+        return r == 100?-1:arr[r++];
+        
+    }
+}
+
