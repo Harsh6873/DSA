331b7d1994b74325c155c8e5d878726668f128a6
Harsh Agarwal <111336536+Harsh6873@users.noreply.github.com>
2023-08-14 20:31:41 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[2 * n + 2];
+            for(int i = 0; i < 2 * n + 2; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = ob.singleNumber(v);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        int nums1[]= new int[2];
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        }       
+
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(mp.get(nums[i])==1){
+                nums1[j]=nums[i];
+                j++;
+            }
+        }
+         Arrays.sort(nums1);
+         return nums1;
+        // Code here
+    }
+}
