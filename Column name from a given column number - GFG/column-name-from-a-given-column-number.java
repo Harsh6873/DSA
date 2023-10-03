b5a2a6593c09341af94710ff728932a2719a1168
Harsh Agarwal <111336536+Harsh6873@users.noreply.github.com>
2023-10-03 21:18:39 +0530

    Added solution - LeetHub

---
+++ Column name from a given column number - GFG/column-name-from-a-given-column-number.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+    		System.out.println (new Solution().colName (n));
+        }
+        
+    }
+}
+
+// Contributed By: Pranay Bansal 
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    String colName (long n)
+    {
+        String str="";
+        while(n > 0){
+            char c = (char)('A'+(n-1)%26);
+            str = c+str;
+            n = (n-1)/26;
+        }
+        return str;
+        // your code here
+    }
+}
